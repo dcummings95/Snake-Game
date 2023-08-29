4541db4a36cb9559b988a2a9d6ac488ad73af5ac
Dominic Cummings <dominiccummings95@gmail.com>
2023-08-29 16:46:06 -0500

    Displaying head and food

---
+++ snake.js
@@ -0,0 +1,35 @@
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20; 
+var board;
+var context;
+
+//snake head (starting point 5,5)
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+//food
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    update();
+}
+
+function update() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
